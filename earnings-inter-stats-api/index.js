6d60e2371315a205ef48cfd89abd0c2f157eaaab
sos1819zgg <zoilo_gomez@hotmail.com>
2019-04-06 14:18:28 +0000

    cambios zoilo

--- earnings-inter-stats-api/index.js
+++ earnings-inter-stats-api/index.js
@@ -100,9 +100,10 @@ earningsInterStatsApi.register = function(app, earningsInterStats) {
                 earningsInterStats.find({"title":title}).toArray((err, earningsInterStatsArray)=>{
                     if(err)
                         console.log("Error: " + err);
-                    var film = earningsInterStatsArray[0];
-                    delete film._id;
-                    res.send(film);
+                    res.send(earningsInterStatsArray.map((c) => {
+                        delete c._id;
+                        return c;
+                    }));
             });
             }else if(territory){
                 earningsInterStats.find({"territory":{$gte:territory}}).toArray((err, earningsInterStatsArray)=>{
