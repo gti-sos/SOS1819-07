13b7d64d5bf0ddc9f32e29926387b7df3f0c9fb6
sos1819zgg <zoilo_gomez@hotmail.com>
2019-04-08 19:41:00 +0000

    minipostman zoilo

---
+++ public/earningsInterStatsApp/main-ctrl.js
@@ -0,0 +1,47 @@
+/* global angular */
+var app = angular.module("EarningsInterStatsApp");
+
+app.controller("MainCtrl", ["$scope", "$http", function ($scope,$http){
+                console.log("Modular MainCtrl initialized");
+                $scope.url = "https://sos1819-07.herokuapp.com/api/v1/earnings-inter-stats/";
+
+                $scope.get = function(){
+                    $http.get($scope.url).then(function(response){
+                        $scope.status = response.status;
+                        $scope.data = JSON.stringify(response.data,null,2);
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+
+                $scope.post = function(){
+                    $http.post($scope.url,$scope.data).then(function(response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+                
+                $scope.put = function(){
+                    $http.put($scope.url,$scope.data).then(function(response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+                
+                $scope.delete = function(){
+                    $http.delete($scope.url).then(function(response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+}]);
