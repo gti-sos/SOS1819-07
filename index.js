a4e0739d0b6dbd9bd6189fe5c8614efbcc14b0ec
sos1819zgg <zoilo_gomez@hotmail.com>
2019-05-14 18:40:59 +0000

    cambios

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var takingstatsApi = require("./takingstats-api");
-app.use("/", express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/public/"));
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
