243595420784e0522ca6ba1041e0609e0d783867
sos1819zgg <zoilo_gomez@hotmail.com>
2019-05-24 23:12:03 +0000

    cors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
 var takingstatsApi = require("./takingstats-api");
-app.use("/", express.static(__dirname + "/public/"));
+var cors = require("cors");
+
+var app = express();
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -21,6 +22,10 @@ var allowCrossDomain = function(req, res, next) {
 app.use(allowCrossDomain);
 
 app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/", express.static(__dirname + "/public/"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(allowCrossDomain);
