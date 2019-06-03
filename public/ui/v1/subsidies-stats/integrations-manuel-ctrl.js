75ca18ddd1abe87c0547a7d63599ede07af4cb9d
sos1819mso <msalasortiz@gmail.com>
2019-06-03 17:38:47 +0000

    final changes

--- public/ui/v1/subsidies-stats/integrations-manuel-ctrl.js
+++ public/ui/v1/subsidies-stats/integrations-manuel-ctrl.js
@@ -10,6 +10,17 @@ app.controller("integrationsManuelCtrl", ["$scope", "$http", function($scope, $h
     
     var apiGB = "/ui/v1/subsidies-stats/GHIBLI";
     
+    var apiQ = "https://breaking-bad-quotes.herokuapp.com/v1/quotes/5";
+    
+    var apiG12 = "https://sos1819-12.herokuapp.com/api/v1/pollution-stats";
+    
+    var apiDC = "https://deckofcardsapi.com/api/deck/new/draw/";
+    
+    var API = "https://sos1819-07.herokuapp.com/api/v2/subsidies-stats";
+    
+    var apiIMDB = "https://www.omdbapi.com/?i=";
+    
+    //USO API G11
     $http.get(apiG11).then(function(response) {
         
     var data = response.data;
@@ -97,6 +108,7 @@ app.controller("integrationsManuelCtrl", ["$scope", "$http", function($scope, $h
 
     });
     
+    //USO API Studio Ghibli
     $http.get(apiGB).then(function (response) {
         
     var data = response.data;
@@ -145,5 +157,142 @@ chart.renderTo('#bar');
 
         
     });
+    
+    //USO API Breaking Bad quotes
+    $http.get(apiQ).then(function(response){
+        
+        $scope.quotes = response.data;
+        
+    });
+    $scope.refresh = function refresh(){ 
+    $http.get(apiQ).then(function(response){
+        
+       $scope.quotes = response.data;
+        
+    });
+    };
+    
+    //USO API G12
+    $http.get(apiG12).then(function(response){
+        
+        var data = response.data;
+    
+    var chardata1 = data.map( function (item)  { 
+        
+        var newItem = item.country;
+        
+        return newItem;
+        
+    } );
+    
+    var chardata2 = data.map( function (item)  { 
+        
+        var newItem = item.year;
+        
+        return newItem;
+        
+    } );
+    
+    var chardata3 = data.map( function (item)  { 
+        
+        var newItem = item.pollution_tco2;
+        
+        return newItem;
+        
+    } );
+        
+        Highcharts.chart('container2', {
+    chart: {
+        type: 'timeline'
+    },
+    xAxis: {
+        visible: false
+    },
+    yAxis: {
+        visible: false
+    },
+    title: {
+        text: 'Timeline of Spain levels of polution'
+    },
+    subtitle: {
+        text: 'Info source: <a href="https://sos1819-12.herokuapp.com/api/v1/pollution-stats/">www.Api-G12/Pollution-stats.com</a>'
+    },
+    colors: [
+        '#4185F3',
+        '#427CDD',
+        '#406AB2',
+        '#3E5A8E',
+        '#3B4A68',
+        '#363C46'
+    ],
+    series: [{
+        data: [{
+            name: chardata2[1],
+            label: chardata1[1]+':Nivel de polución:'+ chardata3[1]
+        },{
+            name: chardata2[2],
+            label: chardata1[2]+':Nivel de polución:'+ chardata3[2]
+        },{
+            name: chardata2[0],
+            label: chardata1[0]+':Nivel de polución:'+ chardata3[0]
+        }]
+    }]
+});
+        
+    });
+    
+    //USO API draw a card
+    $http.get(apiDC).then(function(response){
+        
+        
+        $scope.cards = response.data.cards;
+        
+        console.log($scope.cards);
+        
+    });
+    $scope.refresh2 = function refresh(){ 
+    $http.get(apiDC).then(function(response){
+        
+        
+        
+       $scope.cards = response.data.cards;
+        
+    });
+    
+    };
+    
+    //INTEGRACIÓN API subsidies-stats con API filmaffinity
+    
+    
+    var ID = ["tt3896102", "tt6101820", "tt5176252", "tt5187886", "tt6060156"];
+    var APIKEY = "9ef01c31";
+    $http.get(API).then(function (response){
+        
+        $scope.infos = [];
+        
+        $scope.films = response.data;
+        
+        console.log(response.data.film);
+        
+        
+    
+        
+        for(var i = 0;i<ID.length;i++){
+         
+        $http.get(apiIMDB+ID[i]+"&apikey="+APIKEY).then(function(response){
+            
+            $scope.infos.push(response.data);
+            
+            $scope.data = angular.merge({}, $scope.films, $scope.infos);
+            
+        });
+        
+        
+        
+        }
+        
+    });
+    
+    
     
 }]);
